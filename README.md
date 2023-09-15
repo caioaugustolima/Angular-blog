5189ec0522cb2d3e3b1992dfd4b53241472b654c
caioaugustolima <caiolimadecarvalho@hotmail.com>
2023-09-14 21:30:10 -0300

    inclusão classe para background image

---
+++ README.md
@@ -0,0 +1,58 @@
+# Portal de Notícias - Blog da cerva 🍺
+
+Bem-vindo ao meu projeto de Portal de Notícias sobre o mundo cervejeiro! Este projeto foi desenvolvido como parte do Santander Bootcamp jornada Java + Angular, sob a orientação do professor Felipe Silva Aguiar da DIO.
+
+## 🌎 Sobre o Projeto
+
+O portal é uma plataforma simples, mas eficaz, que concentra notícias do mundo cervejeiro em um único lugar. A principal funcionalidade é a exibição de manchetes na página inicial, e quando os usuários clicam em uma das noticias, eles são direcionados para uma página de conteúdo que exibe a notícia de forma completa. Cada notícia é carregada de forma dinâmica com base em um identificador (ID) conectado a um banco de dados "fake" que contém os dados da notícia. Tudo isso conversando e funcionando por conta da (componentização) proporcionada pelo Angular.
+
+O uso do Angular neste projeto permitiu a criação de uma estrutura altamente modular e componentizada. Isso significa que cada elemento do site, desde o cabeçalho até as seções de notícias individuais, foram encapsulados em componentes separados. Isso trouxe uma série de vantagens:
+
+- **Reusabilidade**: A componentização facilita a reutilização de código, economizando tempo e esforço na replicação de funcionalidades em diferentes partes do site.
+
+- **Manutenção Simplificada**: Com componentes bem definidos, a manutenção e atualização do projeto se tornam muito mais simples, uma vez que as alterações podem ser feitas em um único local.
+
+- **Escalabilidade**: À medida que o projeto cresce, a abordagem de componentização se mostra crucial para manter a organização e o desempenho da aplicação.
+
+- **Arquitetura MVC**: O Angular adota uma arquitetura de Model-View-Controller (MVC) que facilita a organização do código e a separação de preocupações.
+
+- **Facilidade de Testes**: O Angular é altamente testável, permitindo a criação de testes unitários e de integração de forma simples.
+
+## 🖥️ Visualização e Acesso
+
+[Blog da cerva.webm](https://github.com/caioaugustolima/angular-blog/assets/50187646/3e366619-06e1-467c-bae4-b8754139cb20)
+
+
+O projeto também está hospedado no Netlify, proporcionando uma maneira fácil de conferir e acessar o meu portfólio. Você pode visitá-lo [aqui](https://blogdacerva.netlify.app/).
+
+
+## ⏳ Próximas Atualizações
+
+
+Estou trabalhando continuamente no aprimoramento deste portal de notícias. Alguns dos próximos passos incluem:
+
+- **Responsividade**: Estou refinando a responsividade aplicada no projeto e sofrerá novas melhorias conforme evolução do portal.
+
+- **Melhorias na Interface do Usuário**: Vou aprimorar a experiência do usuário com melhorias na interface e na usabilidade.
+
+- **Integração de Recursos Adicionais**: Planejo adicionar recursos como pesquisa de notícias, categorização e compartilhamento nas redes sociais.
+
+Obrigado por visitar o meu projeto! Sinta-se à vontade para explorar o código-fonte e contribuir para o seu desenvolvimento. Suas sugestões e feedback são sempre bem-vindos.
+
+## 🛠️ Construído com
+
+* [HTML](#) - Utilizado para estruturar a página web e seus elementos.
+* [CSS](#) - Responsável pela estilização e aparência visual do portal.
+* [TypeScript](#) - Linguagem de programação que proporciona maior robustez ao JavaScript.
+* [Angular](#) - Um framework poderoso que facilitou a criação da estrutura do projeto, a organização de componentes e a dinamicidade das páginas.
+
+## 📌 Versão
+
+Projeto em sua versão Inicial que sofrerá novas implementações em breve.
+
+## 🎁 Expressões de gratidão
+
+Quero expressar minha gratidão ao professor [Felipe Silva Aguiar](https://www.linkedin.com/in/felipe-exe/) da [DIO](https://www.dio.me/) por sua orientação e expertise, que foram fundamentais para o sucesso deste projeto.
+
+---
+⌨️ desenvolvido por [Caio Lima](https://github.com/caioaugustolima) 😁
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,59 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title":"Mudança climática preocupa produtores de lúpulo e cerveja pode até mudar de gosto",
+    "description1":"Gerações de agricultores nas colinas ensolaradas e verdejantes de Spalt, na Alemanha, cuidam orgulhosamente de suas plantas de lúpulo, usadas na fabricação de cerveja, desde a Idade Média.",
+    "description2":"Quando perguntados sobre o que torna a variedade de lúpulo natural de Spalt tão especial, os entusiastas falam sobre seu aroma delicado e picante; sua leveza; e a harmonia e o toque de amargor que a planta proporciona.",
+    "photoCover": "https://f.i.uol.com.br/fotografia/2023/09/12/16945527566500d2b400be5_1694552756_3x2_md.jpg"
+  },
+  {
+    "id":"2",
+    "title":"Concurso internacional coloca cervejas brasileiras entre as melhores do mundo",
+    "description1":'De Minas para o mundo.  A cerveja artesanal Wälls Brut foi eleita a melhor cerveja do planeta em duas categorias: "Worlds Best Specialty Beer" e "Worlds Best Brut Beer” com estilo Brut. A bebida produzida em Minas é feita pelo método champenoise de refermentação, com a sofisticação das notas cítricas. Chamam a atenção também sua cor dourada e seu sabor leve e delicado.',
+    "description2":'Além da Wälls, a marca Bohemia Reserva conquistou o "Worlds Best Dark Barley Wine” e a Colorado foi contemplada na “Worlds Best Flavoured Honey & Maple” com a cerveja Appia.',
+    "photoCover":"https://www.portaldailha.com.br/noticias/fotos/melhorescervejas01setembro.jpg"
+  },
+
+  {
+    "id":"3",
+    "title":"Cerveja não é saudável como afirma estudo chinês",
+    "description1":"O resultado de um estudo realizado na China deixou os cervejeiros empolgados ao concluir que a bebida possui ingredientes benéficos para a saúde. De acordo com a pesquisa, alguns componentes da cerveja são decompostos pelo microbioma intestinal e, dessa forma, poderiam exercer um efeito regulador no sistema imunológico.",
+    "description2":"Esse efeito seria anti-inflamatório, anticoagulante e antioxidante, tornando inofensivos alguns produtos nocivos gerados no metabolismo.",
+    "photoCover":"https://static.dw.com/image/39970473_354.jpg"
+  },
+  {
+    "id":"4",
+    "title":"Bierland anuncia novidades para Oktoberfest 2023",
+    "description1":'A aposta para alavancar as vendas está nas novidades que a cervejaria vai levar para o evento neste ano. Entre as surpresas que estão sendo preparadas, a cervejaria pretende levar uma variedade de até 12 estilos diferentes de chopes e cervejas. Entre eles, um lançamento da marca: o chope sem álcool.',
+    "description2":"De acordo com a Euromonitor International, a produção de cervejas sem álcool ou com baixo teor alcoólico no país deve chegar a 480 milhões de litros até o final de 2023. No último ano, o país teve um total de 2.5% de consumo de bebidas sem álcool ou com baixo teor alcoólico.",
+    "photoCover":"https://omunicipioblumenau.com.br/wp-content/uploads/2023/09/bierland-anuncia-novidades-para-oktoberfest-2023-whatsapp-image-2023-09-11-at-15.13.06.jpeg"
+  },
+  {
+    "id":"5",
+    "title":"PL de Zema que eleva imposto de cerveja, ração e celular já pode ser votado",
+    "description1":'Já está pronto para ser votado em primeiro turno o projeto de lei (PL) do governador Romeu Zema (Novo) que aumenta em 2 pontos percentuais o Imposto Sobre Circulação de Mercadorias e Serviços (ICMS) de produtos considerados supérfluos para o estado, como cerveja, ração para pets e telefones celulares, passando a tributação dos atuais 25% para 27%.',
+    "description2":'Apesar de a proposição ter sido aprovada nesta terça (12/9) na Comissão de Fiscalização Financeira e Orçamentária da Assembleia Legislativa, o PL recebeu críticas até mesmo da base de Zema e o governo deve enfrentar dificuldade em aprovar a proposta, que também não conta com o aval da Federação das Indústrias de Minas Gerais (Fiemg), contrária ao aumento da tributação. A oposição do governo na Assembleia Legislativa de Minas Gerais (ALMG) também anunciou que vai obstruir a votação para debater o assunto com a população.',
+    "photoCover":"https://i.em.com.br/2Z_yiKssOZ1d4lFaLu69HNfkVvA=/820x0/smart/imgsapp.em.com.br/app/noticia_127983242361/2023/09/12/1560751/na-sala-de-reuniao-da-almg-pessoas-seguram-cartazes-em-protesto-contra-aumento-de-tributos-proposto-pelo-governo-zema_1_126391.jpg"
+  },
+  {
+    "id":"6",
+    "title":"Lager, Ale, Sour: qual a diferença entre os tipos de cerveja?",
+    "description1":'Se engana quem acha que cerveja é tudo igual. Bebida preferida dos brasileiros, a cerveja vai muito além dos rótulos das marcas que encontramos no supermercado. Cervejarias artesanais estão crescendo no país, e não é de hoje que o público está abrindo os olhos para a incontável variedade de sabores e tipos de cerveja.',
+    "description2":'As principais diferenças (além do sabor) estão no preparo: tipos de leveduras, temperaturas de fermentação e nível de torra do malte são alguns dos fatores que caracterizam cada tipo de cerveja. Para simplificar, chamamos o Rodrigo Nogueira, cervejeiro e sócio da Croma Beer, que explicou as particularidades dos principais tipos de cerveja - e deu dicas de como harmonizá-las.',
+    "photoCover":"https://p2.trrsf.com/image/fget/cf/774/0/images.terra.com/2023/08/04/istock-1091469178-vbheq9q8exip.jpg"
+  },
+  {
+    "id":"7",
+    "title":"Heineken e Adidas lançam tênis pelos 150 anos da cerveja",
+    "description1":'Para dar continuidade às comemorações de 150 anos da marca, a Heineken anuncia peças exclusivas e limitadas em parceria com a Adidas. A campanha de aniversário celebra os Good Times, que enaltecem como Heineken busca sempre se conectar com seus consumidores e destacar como, ao redor do mundo, seu público encontra maneiras únicas de pronunciar o nome da marca e desfrutar seus bons momentos com a bebida.',
+    "description2":'As peças adidas customizadas são jaquetas e tênis, e têm a identidade visual da comemoração dos 150 anos de Heineken. Os itens não estão disponíveis para venda ao público geral, mas fazem parte de ação especial com super fãs da marca, além de serem distribuídos e usados por personalidades influentes de diferentes áreas de atuação, como entretenimento, moda e arte.',
+    "photoCover":"https://www.mundodomarketing.com.br/wp-content/uploads/2023/09/Heineken-e-Adidas-lancam-tenis-pelos-150-anos-da-cerveja-jpg.webp"
+  },
+  {
+    "id":"8",
+    "title":"Conheça a cerveja criada para homenagear a cidade de São Vicente",
+    "description1":'O município serviu de inspiração para uma cervejaria que criou essa bebida de nome Bartira, pra homenagear a primeira cidade do Brasil. Foram necessários 25 dias pra ficar pronta.',
+    "description2":'Conheça a micro cervejaria artesanal responsável pelo lançamento, a ideia partiu do proprietário em conjunto com a secretária de desenvolvimento econômico da cidade. A cerveja tem aroma de cravo e banana, e já está à venda em quatro estabelecimentos. A reportagem é de Fabio Garcez e as imagens são de Paulo Barrio.',
+    "photoCover":"https://blogdobaena.com.br/wp-content/uploads/2023/08/cerveja-bartira.png"
+  }
+]
